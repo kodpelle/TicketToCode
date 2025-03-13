85f47d1f98ccd3df1beb84d4326e87d9f7420895
kodpelle <pellewiklund@outlook.com>
2025-03-13 11:53:50 +0100

    Skapade en enkel CreateEvent sida

--- TicketToCodeBlazor/EventDto.cs
+++ TicketToCodeBlazor/EventDto.cs
@@ -6,7 +6,7 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public EventType type { get; set; } = EventType.Other;
+        public EventType Type { get; set; } = EventType.Other;
         public DateTime Start{ get; set; }
         public DateTime End { get; set; }
         public int MaxAttendees { get; set; }
--- TicketToCodeBlazor/Program.cs
+++ TicketToCodeBlazor/Program.cs
@@ -17,6 +17,7 @@ namespace TicketToCodeBlazor
 
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<EventService>();
 
             await builder.Build().RunAsync();
         }
