da59e640b5e368415371146a6c88c140212a7699
AmZu2024 <amar.zukanovic57@gmail.com>
2025-03-17 16:27:15 +0100

    Skapat endpoints

---
+++ TicketToCode.Api/Endpoints/Event/Update.cs
@@ -0,0 +1,32 @@
+﻿namespace TicketToCode.Api.Endpoints;
+
+public class Update : IEndpoint
+{
+    public static void MapEndpoint(IEndpointRouteBuilder app) => app
+        .MapPut("/events/{id}", Handle)
+        .WithSummary("Update an event");
+    private static async Task<IResult> Handle(int id,UpdateEvent request, ApplicationDBContext db)
+    {
+        var ev = await db.Events.FindAsync(id);
+        if (ev == null)
+        {
+            return TypedResults.NotFound("Event not found");
+        }
+        ev.Name = request.Name;
+        ev.Description = request.Description;
+        ev.Type = request.Type;
+        ev.StartTime = request.Start;
+        ev.EndTime = request.End;
+        ev.MaxAttendees = request.MaxAttendees;
+        await db.SaveChangesAsync();
+        return TypedResults.Ok();
+    }
+    public record UpdateEvent(
+        string Name,
+        string Description,
+        EventType Type,
+        DateTime Start,
+        DateTime End,
+        int MaxAttendees
+    );
+}
